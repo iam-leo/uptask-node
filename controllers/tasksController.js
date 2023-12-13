bc47569d11326fc72cc56acbf56a03979bf90ad8
Leo Ss <leo.vm.cba@gmail.com>
2023-12-13 10:52:13 -0300

    add: new task validations

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -3,9 +3,23 @@ const tasksHome = (req, res) => {
 }
 
 const newTask = (req, res) => {
+    // Validar que el input contenga algo
     const {task} = req.body
-    console.log( task );
-    res.send('New task created!');
+
+    let errores = [];
+
+    if (!task) {
+        errores.push({message: 'El campo task es requerido'});
+    }
+
+    //Si hay errores
+    if (errores.length > 0) {
+        res.status(400).send(errores);
+    } else{
+        // No hay errores. Insertar en la DB
+        
+        res.status(200).send({message: 'Se ha creado la tarea correctamente'});
+    }
 }
 
 export {
