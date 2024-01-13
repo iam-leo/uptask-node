c7615b7a123b4b3dfcb47ac73ed235fc10d6228c
Leo Ss <leo.vm.cba@gmail.com>
2024-01-12 21:52:57 -0300

    add: new-user form validations

--- frontend/src/app/components/new-user/new-user.component.ts
+++ frontend/src/app/components/new-user/new-user.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AccountService } from '../../services/account.service';
 
 @Component({
@@ -14,10 +14,18 @@ export class NewUserComponent {
   name = '';
   email = '';
   password = '';
+  errorStatus = false;
+  errorMessage = '';
+  successStatus = false;
 
-  constructor( private _accountService: AccountService ) {}
+  constructor( private _accountService: AccountService, private router: Router ) {}
 
   createAccount(name: string, email: string, password: string){
+    // Validamos si estan todos los campos requeridos sino paramos la ejecucion
+    if(!this.validateName(name) || !this.validateEmail(email) || !this.validatePassword(password)){
+      return
+    }
+
     const user = {
       name,
       email,
@@ -25,8 +33,65 @@ export class NewUserComponent {
     }
 
     this._accountService.newAccount(user).subscribe(()=>{
-      console.log('User creado');
+      this.successStatus = true;
+      setTimeout(() => {
+        this.successStatus = false;
+        this.router.navigate(['/']);
+
+      },3000);
     })
+  }
+
+  validateName(name: string){
+    if(!name || name.trim() === ''){
+      this.errorMessage = 'El nombre es requerido.';
+      this.errorStatus = true;
+      this.hideError();
+      return false;
+    }else{
+      return true
+    }
+  }
+
+  validateEmail(email: string){
+    if (!email || email.trim() === '') {
+      this.errorMessage = 'El email es requerido.';
+      this.errorStatus = true;
+      this.hideError();
+      return false;
+    } else if (!this.isValidEmail(email)) {
+      this.errorMessage = 'Por favor, ingresa un email válido.';
+      this.errorStatus = true;
+      this.hideError();
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  isValidEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  validatePassword(password: string){
+    if (!password || password.trim() === '') {
+      this.errorMessage = 'La contraseña es requerida.';
+      this.errorStatus = true;
+      this.hideError();
+      return false;
+    } else if (password.length < 6) {
+      this.errorMessage= 'La contraseña debe tener al menos 6 caracteres.';
+      this.errorStatus = true;
+      this.hideError();
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  hideError(){
+    setTimeout(()=> this.errorStatus=false ,3000)
   }
 
 }
